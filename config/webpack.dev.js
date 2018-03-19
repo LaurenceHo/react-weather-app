d693f6cc7d6747a90c1829e41ff06a8e6b674939
Laurence Ho <bluegray.he@aol.com>
2018-03-20 09:31:20 +1300

    Minor change

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge (common, {
 	mode: 'development',
 	devtool: 'inline-source-map',
 	devServer: {
-		contentBase: './dist',
+		contentBase: '../dist',
 		historyApiFallback: true,
 		hot: true,
 		inline: true
