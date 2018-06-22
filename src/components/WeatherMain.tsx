22017b0b74ca3887fa4dd5b21ea40b731bf5f79d
Laurence Ho <bluegray.he@aol.com>
2018-06-22 15:23:23 +1200

    Create data model for mapping weather data

--- src/components/WeatherMain.tsx
+++ src/components/WeatherMain.tsx
@@ -5,70 +5,12 @@ import { connect } from 'react-redux';
 import { Alert, Card, Col, Row, Spin } from 'antd';
 
 import { fetchingData, fetchingDataFailure, fetchingDataSuccess, setAllWeatherDataIntoStore } from '../redux/actions';
+import { Forecast, Timezone } from './DataModel';
 import WeatherForecast from './WeatherForecast';
 
 import {
 	getGeocode, getWeather
 } from '../api';
-
-interface Timezone {
-	timezone: string,
-	offset: number
-}
-
-interface Weather {
-	time: number,
-	summary: string,
-	icon: string,
-	nearestStormDistance: number,
-	precipIntensity: number,
-	precipIntensityMax: number,
-	precipIntensityMaxTime: number,
-	precipProbability: number,
-	precipType: string,
-	temperature: number,
-	apparentTemperature: number,
-	temperatureHigh: number,
-	temperatureHighTime: number,
-	temperatureLow: number,
-	temperatureLowTime: number,
-	apparentTemperatureHigh: number,
-	apparentTemperatureHighTime: number,
-	apparentTemperatureLow: number,
-	apparentTemperatureLowTime: number,
-	dewPoint: number,
-	humidity: number,
-	pressure: number,
-	windSpeed: number,
-	windGust: number,
-	cloudCover: number,
-	uvIndex: number,
-	visibility: number
-}
-
-interface Forecast {
-	latitude: number,
-	longitude: number,
-	timezone: string,
-	currently: Weather,
-	minutely: {
-		summary: string,
-		icon: string,
-		data: Weather[],
-	}
-	hourly: {
-		summary: string,
-		icon: string,
-		data: Weather[]
-	},
-	daily: {
-		summary: string,
-		icon: string,
-		data: Weather[]
-	}
-	flags: Object,
-	offset: number
-}
 
 class WeatherMain extends React.Component<any, any> {
 	constructor(props: any) {
