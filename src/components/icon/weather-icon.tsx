252caa46072c02c6e50f7faaa465624d4303401f
Laurence Ho <bluegray.he@aol.com>
2020-06-07 10:09:30 +1200

    Convert to react hook

--- src/components/icon/weather-icon.tsx
+++ src/components/icon/weather-icon.tsx
@@ -6,37 +6,34 @@ interface WeatherIconProps {
   size?: string;
 }
 
-export class WeatherIcon extends React.Component<WeatherIconProps, any> {
-  render() {
-    const { icon, size } = this.props;
-    const defaultSize = !size ? '1rem' : size;
+export const WeatherIcon: React.FC<WeatherIconProps> = ({ icon, size }: WeatherIconProps) => {
+  const defaultSize = !size ? '1rem' : size;
 
-    const renderIcon = () => {
-      if (icon === Condition.CLEAR_DAY) {
-        return <i className='wi wi-day-sunny' />;
-      } else if (icon === Condition.CLEAR_NIGHT) {
-        return <i className='wi wi-night-clear' />;
-      } else if (icon === Condition.RAIN) {
-        return <i className='wi wi-rain' />;
-      } else if (icon === Condition.SNOW) {
-        return <i className='wi wi-snow' />;
-      } else if (icon === Condition.SLEET) {
-        return <i className='wi wi-sleet' />;
-      } else if (icon === Condition.WIND) {
-        return <i className='wi wi-windy' />;
-      } else if (icon === Condition.FOG) {
-        return <i className='wi wi-fog' />;
-      } else if (icon === Condition.CLOUDY) {
-        return <i className='wi wi-cloudy' />;
-      } else if (icon === Condition.PARTLY_CLOUDY_DAY) {
-        return <i className='wi wi-day-cloudy' />;
-      } else if (icon === Condition.PARTLY_CLOUDY_NIGHT) {
-        return <i className='wi wi-night-alt-cloudy' />;
-      } else {
-        return null;
-      }
-    };
+  const renderIcon = () => {
+    if (icon === Condition.CLEAR_DAY) {
+      return <i className='wi wi-day-sunny' />;
+    } else if (icon === Condition.CLEAR_NIGHT) {
+      return <i className='wi wi-night-clear' />;
+    } else if (icon === Condition.RAIN) {
+      return <i className='wi wi-rain' />;
+    } else if (icon === Condition.SNOW) {
+      return <i className='wi wi-snow' />;
+    } else if (icon === Condition.SLEET) {
+      return <i className='wi wi-sleet' />;
+    } else if (icon === Condition.WIND) {
+      return <i className='wi wi-windy' />;
+    } else if (icon === Condition.FOG) {
+      return <i className='wi wi-fog' />;
+    } else if (icon === Condition.CLOUDY) {
+      return <i className='wi wi-cloudy' />;
+    } else if (icon === Condition.PARTLY_CLOUDY_DAY) {
+      return <i className='wi wi-day-cloudy' />;
+    } else if (icon === Condition.PARTLY_CLOUDY_NIGHT) {
+      return <i className='wi wi-night-alt-cloudy' />;
+    } else {
+      return null;
+    }
+  };
 
-    return <span style={{ fontSize: defaultSize }}>{renderIcon()}</span>;
-  }
-}
+  return <span style={{ fontSize: defaultSize }}>{renderIcon()}</span>;
+};
