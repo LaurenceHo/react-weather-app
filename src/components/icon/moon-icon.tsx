c4cbb06bdba70ade0b71e38b48ff425e01ed6d27
Laurence Ho <bluegray.he@aol.com>
2020-06-07 10:09:30 +1200

    Convert to react hook

--- src/components/icon/moon-icon.tsx
+++ src/components/icon/moon-icon.tsx
@@ -6,79 +6,76 @@ interface MoonIconProps {
   size: string;
 }
 
-export class MoonIcon extends React.Component<MoonIconProps, any> {
-  render() {
-    const { moonPhase, latitude, size } = this.props;
-    const moonPhaseCalculated = Math.round((moonPhase * 100) / 3.57);
+export const MoonIcon: React.FC<MoonIconProps> = ({ moonPhase, latitude, size }: MoonIconProps) => {
+  const moonPhaseCalculated = Math.round((moonPhase * 100) / 3.57);
 
-    const renderMoonIcon = () => {
-      if (latitude > 0) {
-        if (Math.floor(moonPhaseCalculated / 7) === 0) {
-          if (moonPhaseCalculated === 0) {
-            return <i className='wi wi-moon-alt-new' />;
-          } else {
-            const className = `wi wi-moon-alt-waxing-crescent-${moonPhaseCalculated % 7}`;
-            return <i className={className} />;
-          }
-        } else if (Math.floor(moonPhaseCalculated / 7) === 1) {
-          if (moonPhaseCalculated === 7) {
-            return <i className='wi wi-moon-alt-first-quarter' />;
-          } else {
-            const className = `wi wi-moon-alt-waxing-gibbous-${moonPhaseCalculated % 7}`;
-            return <i className={className} />;
-          }
-        } else if (Math.floor(moonPhaseCalculated / 7) === 2) {
-          if (moonPhaseCalculated === 14) {
-            return <i className='wi wi-moon-alt-full' />;
-          } else {
-            const className = `wi wi-moon-alt-waning-gibbous-${moonPhaseCalculated % 7}`;
-            return <i className={className} />;
-          }
-        } else if (Math.floor(moonPhaseCalculated / 7) === 3) {
-          if (moonPhaseCalculated === 21) {
-            return <i className='wi wi-moon-alt-third-quarter' />;
-          } else {
-            const className = `wi wi-moon-alt-waning-crescent-${moonPhaseCalculated % 7}`;
-            return <i className={className} />;
-          }
-        } else if (moonPhaseCalculated / 7 === 4) {
+  const renderMoonIcon = () => {
+    if (latitude > 0) {
+      if (Math.floor(moonPhaseCalculated / 7) === 0) {
+        if (moonPhaseCalculated === 0) {
           return <i className='wi wi-moon-alt-new' />;
+        } else {
+          const className = `wi wi-moon-alt-waxing-crescent-${moonPhaseCalculated % 7}`;
+          return <i className={className} />;
         }
-      } else {
-        if (Math.floor(moonPhaseCalculated / 7) === 0) {
-          if (moonPhaseCalculated === 0) {
-            return <i className='wi wi-moon-alt-new' />;
-          } else {
-            const className = `wi wi-moon-alt-waning-crescent-${7 - (moonPhaseCalculated % 7)}`;
-            return <i className={className} />;
-          }
-        } else if (Math.floor(moonPhaseCalculated / 7) === 1) {
-          if (moonPhaseCalculated === 7) {
-            return <i className='wi wi-moon-alt-third-quarter' />;
-          } else {
-            const className = `wi wi-moon-alt-waning-gibbous-${7 - (moonPhaseCalculated % 7)}`;
-            return <i className={className} />;
-          }
-        } else if (Math.floor(moonPhaseCalculated / 7) === 2) {
-          if (moonPhaseCalculated === 14) {
-            return <i className='wi wi-moon-alt-full' />;
-          } else {
-            const className = `wi wi-moon-alt-waxing-gibbous-${7 - (moonPhaseCalculated % 7)}`;
-            return <i className={className} />;
-          }
-        } else if (Math.floor(moonPhaseCalculated / 7) === 3) {
-          if (moonPhaseCalculated === 21) {
-            return <i className='wi wi-moon-alt-first-quarter' />;
-          } else {
-            const className = `wi wi-moon-alt-waxing-crescent-${7 - (moonPhaseCalculated % 7)}`;
-            return <i className={className} />;
-          }
-        } else if (moonPhaseCalculated / 7 === 4) {
-          return <i className='wi wi-moon-alt-new' />;
+      } else if (Math.floor(moonPhaseCalculated / 7) === 1) {
+        if (moonPhaseCalculated === 7) {
+          return <i className='wi wi-moon-alt-first-quarter' />;
+        } else {
+          const className = `wi wi-moon-alt-waxing-gibbous-${moonPhaseCalculated % 7}`;
+          return <i className={className} />;
         }
+      } else if (Math.floor(moonPhaseCalculated / 7) === 2) {
+        if (moonPhaseCalculated === 14) {
+          return <i className='wi wi-moon-alt-full' />;
+        } else {
+          const className = `wi wi-moon-alt-waning-gibbous-${moonPhaseCalculated % 7}`;
+          return <i className={className} />;
+        }
+      } else if (Math.floor(moonPhaseCalculated / 7) === 3) {
+        if (moonPhaseCalculated === 21) {
+          return <i className='wi wi-moon-alt-third-quarter' />;
+        } else {
+          const className = `wi wi-moon-alt-waning-crescent-${moonPhaseCalculated % 7}`;
+          return <i className={className} />;
+        }
+      } else if (moonPhaseCalculated / 7 === 4) {
+        return <i className='wi wi-moon-alt-new' />;
       }
-    };
+    } else {
+      if (Math.floor(moonPhaseCalculated / 7) === 0) {
+        if (moonPhaseCalculated === 0) {
+          return <i className='wi wi-moon-alt-new' />;
+        } else {
+          const className = `wi wi-moon-alt-waning-crescent-${7 - (moonPhaseCalculated % 7)}`;
+          return <i className={className} />;
+        }
+      } else if (Math.floor(moonPhaseCalculated / 7) === 1) {
+        if (moonPhaseCalculated === 7) {
+          return <i className='wi wi-moon-alt-third-quarter' />;
+        } else {
+          const className = `wi wi-moon-alt-waning-gibbous-${7 - (moonPhaseCalculated % 7)}`;
+          return <i className={className} />;
+        }
+      } else if (Math.floor(moonPhaseCalculated / 7) === 2) {
+        if (moonPhaseCalculated === 14) {
+          return <i className='wi wi-moon-alt-full' />;
+        } else {
+          const className = `wi wi-moon-alt-waxing-gibbous-${7 - (moonPhaseCalculated % 7)}`;
+          return <i className={className} />;
+        }
+      } else if (Math.floor(moonPhaseCalculated / 7) === 3) {
+        if (moonPhaseCalculated === 21) {
+          return <i className='wi wi-moon-alt-first-quarter' />;
+        } else {
+          const className = `wi wi-moon-alt-waxing-crescent-${7 - (moonPhaseCalculated % 7)}`;
+          return <i className={className} />;
+        }
+      } else if (moonPhaseCalculated / 7 === 4) {
+        return <i className='wi wi-moon-alt-new' />;
+      }
+    }
+  };
 
-    return <div style={{ fontSize: size }}>{renderMoonIcon()}</div>;
-  }
-}
+  return <div style={{ fontSize: size }}>{renderMoonIcon()}</div>;
+};
--- src/components/icon/wind-icon.tsx
+++ src/components/icon/wind-icon.tsx
@@ -6,51 +6,48 @@ interface WindIconProps {
   size?: string;
 }
 
-export class WindIcon extends React.Component<WindIconProps, any> {
-  render() {
-    const { degree, size } = this.props;
-    const defaultSize = !size ? '1rem' : size;
+export const WindIcon: React.FC<WindIconProps> = ({ degree, size }: WindIconProps) => {
+  const defaultSize = !size ? '1rem' : size;
 
-    const windCode = Math.round(degree / 22.5);
+  const windCode = Math.round(degree / 22.5);
 
-    const windIcon = () => {
-      if (windCode === Condition.WIND_N) {
-        return <i className='wi wi-wind wi-towards-s' />;
-      } else if (windCode === Condition.WIND_NNE) {
-        return <i className='wi wi-wind wi-towards-ssw' />;
-      } else if (windCode === Condition.WIND_NE) {
-        return <i className='wi wi-wind wi-towards-sw' />;
-      } else if (windCode === Condition.WIND_ENE) {
-        return <i className='wi wi-wind wi-towards-wsw' />;
-      } else if (windCode === Condition.WIND_E) {
-        return <i className='wi wi-wind wi-towards-w' />;
-      } else if (windCode === Condition.WIND_ESE) {
-        return <i className='wi wi-wind wi-towards-wnw' />;
-      } else if (windCode === Condition.WIND_SE) {
-        return <i className='wi wi-wind wi-towards-nw' />;
-      } else if (windCode === Condition.WIND_SSE) {
-        return <i className='wi wi-wind wi-towards-nnw' />;
-      } else if (windCode === Condition.WIND_S) {
-        return <i className='wi wi-wind wi-towards-n' />;
-      } else if (windCode === Condition.WIND_SSW) {
-        return <i className='wi wi-wind wi-towards-nne' />;
-      } else if (windCode === Condition.WIND_SW) {
-        return <i className='wi wi-wind wi-towards-ne' />;
-      } else if (windCode === Condition.WIND_WSW) {
-        return <i className='wi wi-wind wi-towards-ene' />;
-      } else if (windCode === Condition.WIND_W) {
-        return <i className='wi wi-wind wi-towards-e' />;
-      } else if (windCode === Condition.WIND_WNW) {
-        return <i className='wi wi-wind wi-towards-ese' />;
-      } else if (windCode === Condition.WIND_NW) {
-        return <i className='wi wi-wind wi-towards-se' />;
-      } else if (windCode === Condition.WIND_NNW) {
-        return <i className='wi wi-wind wi-towards-sse' />;
-      } else {
-        return null;
-      }
-    };
+  const windIcon = () => {
+    if (windCode === Condition.WIND_N) {
+      return <i className='wi wi-wind wi-towards-s' />;
+    } else if (windCode === Condition.WIND_NNE) {
+      return <i className='wi wi-wind wi-towards-ssw' />;
+    } else if (windCode === Condition.WIND_NE) {
+      return <i className='wi wi-wind wi-towards-sw' />;
+    } else if (windCode === Condition.WIND_ENE) {
+      return <i className='wi wi-wind wi-towards-wsw' />;
+    } else if (windCode === Condition.WIND_E) {
+      return <i className='wi wi-wind wi-towards-w' />;
+    } else if (windCode === Condition.WIND_ESE) {
+      return <i className='wi wi-wind wi-towards-wnw' />;
+    } else if (windCode === Condition.WIND_SE) {
+      return <i className='wi wi-wind wi-towards-nw' />;
+    } else if (windCode === Condition.WIND_SSE) {
+      return <i className='wi wi-wind wi-towards-nnw' />;
+    } else if (windCode === Condition.WIND_S) {
+      return <i className='wi wi-wind wi-towards-n' />;
+    } else if (windCode === Condition.WIND_SSW) {
+      return <i className='wi wi-wind wi-towards-nne' />;
+    } else if (windCode === Condition.WIND_SW) {
+      return <i className='wi wi-wind wi-towards-ne' />;
+    } else if (windCode === Condition.WIND_WSW) {
+      return <i className='wi wi-wind wi-towards-ene' />;
+    } else if (windCode === Condition.WIND_W) {
+      return <i className='wi wi-wind wi-towards-e' />;
+    } else if (windCode === Condition.WIND_WNW) {
+      return <i className='wi wi-wind wi-towards-ese' />;
+    } else if (windCode === Condition.WIND_NW) {
+      return <i className='wi wi-wind wi-towards-se' />;
+    } else if (windCode === Condition.WIND_NNW) {
+      return <i className='wi wi-wind wi-towards-sse' />;
+    } else {
+      return null;
+    }
+  };
 
-    return <span style={{ fontSize: defaultSize }}>{windIcon()}</span>;
-  }
-}
+  return <span style={{ fontSize: defaultSize }}>{windIcon()}</span>;
+};
